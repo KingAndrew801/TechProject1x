b2bd1b465756606eb1e3e1b8a2ac7abd7167c09e
KingAndrew801 <111556315+KingAndrew801@users.noreply.github.com>
2022-09-06 21:26:06 -0600

    changed loops and try blocks
    try block was within while loop, left indented the block with prompt to ask if willing to play again.

--- RandomGame.py
+++ RandomGame.py
@@ -18,39 +18,36 @@ def startgame():
         while gamerunning == True:
             try:
                 guess = int(input("What's your guess?   "))
+                if guess > randnum:
+                    print("Go lower")
+                    score += 1
+                if guess < randnum:
+                    print('Go higher.')
+                    score += 1
+                if guess == randnum:
+                    print('you got it!')
+                    print(f'Game is over bro, you score is {score}')
+                    gamerunning = False
+                    if score < highscore:
+                        highscore = score
+                        print('You got high score!')
+                        score = 1
+                        down = None
             except ValueError:
                 print('guess with numbers dude')
-
-            if guess > randnum:
-                print("Go lower")
-                score += 1
-            if guess < randnum:
-                print('Go higher.')
-                score += 1
-            if guess == randnum:
-                print('you got it!')
-                print(f'Game is over bro, you score is {score}')
+        try:
+            down = input("Do you want to play again?  ")
+            if down != 'y' or 'n':
+                raise Exception('Thats not a valid choice, homie!')
+            if down == 'n':
+                print(f'Fine, game is over, your highscore is {highscore}')
                 gamerunning = False
-                if score < highscore:
-                    highscore = score
-                    print('You got high score!')
-                    score = 1
-                    down = None
-            while down == None:
-                try:
-                    down = input("Do you want to play again?  ")
-                    if down != 'y' or 'n':
-                        raise Exception('Thats not a valid choice, homie!')
-                except Exception as e:
-                    print(e)
-                else:
-                    if down == 'n':
-                        print(f'Fine, game is over, your highscore is {higscore}')
-                        gamerunning = False
-                        break
-                    if down == 'y':
-                        gamerunning = False
-                        break
+                break
+            if down == 'y':
+                gamerunning = False
+                break
+        except Exception as e:
+            print(e)
 
 
 
